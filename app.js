dc4035b8c196464c3538d988c62a869a48c60905
Jeff Losee <jlosee@cricut.com>
2017-01-02 11:28:41 -0700

    Remove DB

--- app.js
+++ app.js
@@ -5,15 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://shop:shop123!@ds155727.mlab.com:55727/customs");
-
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", function(callback) {
-    console.log("Connection succeeded.");
-});
-
 
 var index = require('./routes/index');
 var users = require('./routes/users');
